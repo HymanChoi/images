00a7c48ba124aba3029ed2ccd60f4f89bd4e1ab4
HymanChoi <HymanChoi@outlook.com>
2020-05-18 22:30:05 +0800

    updated

--- src/js/script.js
+++ src/js/script.js
@@ -49,6 +49,30 @@ clickBtn = type => {
                 console.log(err);
             })
             break;
+        case 'crop':
+            console.log('crop');
+            break;
+        case 'zoom':
+            console.log('zoom');
+            break;
+        case 'flip':
+            console.log('flip');
+            break;
+        case 'rotation':
+            console.log('rotation');
+            break;
+        case 'color':
+            console.log('color');
+            break;
+        case 'text':
+            console.log('text');
+            break;
+        case 'icon':
+            console.log('icon');
+            break;
+        case 'mosaic':
+            console.log('mosaic');
+            break;
     }
 }
 
